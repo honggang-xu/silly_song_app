35530ded6a944c957b028ccf9145ade4e9b5764d
Honggang <xhg7@hotmail.com>
2019-02-06 20:43:30 +1300

    Initial Commit

---
+++ Silly SongTests/Silly_SongTests.swift
@@ -0,0 +1,34 @@
+//
+//  Silly_SongTests.swift
+//  Silly SongTests
+//
+//  Created by Honggang on 6/02/19.
+//  Copyright © 2019 Honggang. All rights reserved.
+//
+
+import XCTest
+@testable import Silly_Song
+
+class Silly_SongTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
